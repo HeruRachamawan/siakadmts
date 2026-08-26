8a0cc7ec60ad9147d97c8dc318633b3e0a920217
HeruRachamawan <herurachamawan@gmail.com>
2026-08-26 22:37:51 +0700

    feat(deploy): add backup sync-deploy endpoint and trigger workflow update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/sync-deploy/{key}', function ($key) {
+    if ($key !== 'masnomo2026siakad') {
+        abort(403);
+    }
+    $output = [];
+    exec('git fetch origin main && git reset --hard origin/main && php artisan optimize:clear 2>&1', $output, $code);
+    return response()->json([
+        'code' => $code,
+        'output' => $output
+    ]);
+});
+
 Route::get('/', function () {
     return view('app');
 });
